fdd526b575374b81e65234ee761ff56b94cc8b6a
webgzh907247189 <18255106634@163.com>
2018-12-06 20:41:13 +0800

    update 加密
    update 加密

--- node/decipher.js
+++ node/decipher.js
@@ -13,3 +13,37 @@ function decipher(encrypted) {
 }
 
 module.exports = decipher;
+
+
+
+
+
+
+
+const crypto = require('crypto')
+
+function encrypt(data, key){
+  let cipher = crypto.createCipher('aes192', key);
+  let crypted = cipher.update(data, 'utf8', 'hex');
+  crypted += cipher.final('hex');
+  return crypted;
+}
+
+function decrypt(encrypted, key){
+  let decipher = crypto.createDecipher('aes192', key);
+  let decrypted = decipher.update(encrypted, 'hex', 'utf8');
+  decrypted += decipher.final('utf8');
+  return decrypted;
+}
+
+let key = 'bz'
+
+
+let value = '123456!@#%QWERqwer，。'
+let cryptoValue = encrypt(encodeURIComponent(value),key)
+console.log(cryptoValue,'  11')
+
+//390cfe3e1ab272ed90889843cd46d5f08464b911fefd64793bfdf0820f1043248757cfc29a2a20ad42bacfb6b4b30f2d
+
+let value = decodeURIComponent(decrypt(cryptoValue,key))
+console.log(value,'  22')
